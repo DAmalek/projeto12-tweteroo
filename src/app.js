e52caaf99fa91cbac0bdcc5ec8cea714bf55a770
Daniel Abdel <danielmalek101@gmail.com>
2023-01-09 16:05:33 -0300

    ultimo ajuste

--- src/app.js
+++ src/app.js
@@ -1,53 +1,72 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
+ 
+const app = express();
 
-const app = express()
-
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-const tweets= [];
+const tweets = [];
 const users = [];
 
-app.post('/sign-up', (req, res) =>{
-    let {username, avatar} = req.body;
-    const newUser = {
+
+app.post('/sign-up', (req, res) => {
+    const { username, avatar} = req.body;
+
+    // if (!username || !avatar){
+    //     res.status(400).send({ message: "Insira todos os campos namoral" });
+    //     return;
+    // }
+
+    const novoUser = {
         username,
-        avatar
-    }
-    users.push(newUser);
-
-    res.send("OK")
-});
-
-app.post('/tweets', (req,res) => {
-    let {username, tweet} = req.body;
-    const userAvatar = users.find(u => u.username === username)
-
-    if (!users.find(username)) {
-        res.send("UNAUTHORIZED")
+        avatar,
     }
 
-    const newTweet = {
+    users.push(novoUser);
+    res.status(201).send('vai la usuario!')
+})
+
+
+
+app.post('/tweets', (req, res) => {
+    const { username, tweet} = req.body;
+
+    if (!username || !tweet) {
+      res.status(400).send({ message: "Insira todos os campos namoral" });
+      return;
+    }
+
+    const novoTweet = {
         username,
         tweet,
-        avatar: userAvatar.avatar
     }
 
-    tweets.push(newTweet);
-
-    res.send('OK')
+    tweets.push(novoTweet);
+    res.status(201).send('tweets enviado')
 })
 
 app.get('/tweets', (req, res) => {
-    if (tweets.length > 10) {
-        const tenTweets = tweets.reverse.slice(0,9);
-        res.send(tenTweets);
+    const lastTweets = [];
+    for (let i = 10; i > 0; i--) {
+      const lTweet = tweets[tweets.length - i];
+      if (lTweet !== null && lTweet !== undefined) {
+        const userAvatar = users.find(u => u.username === lTweet.username);
+        const fullTweet = {
+            username: lTweet.username,
+            avatar: userAvatar.avatar,
+            tweet: lTweet.tweet
+        }
+        lastTweets.push(fullTweet);
+      }
     }
 
-    res.send(tweets)
-})
+    res.send(lastTweets);
 
-app.listen(5000, () =>{
-    console.log("server running on port 5000")
+})
+''
+app.listen(5000, () => {
+console.log('server running in port 5000')
+
+
 });
